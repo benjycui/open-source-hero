37ff3e40228627a8c013ffa744a5f1f4503659db
Benjy Cui <benjytrys@gmail.com>
2017-03-15 14:11:51 +0800

    Let there be open-source-hero

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # open-source-hero
+
 A CLI tool to get how many repositories a user had contributed to.
+
+## Usage
+
+Install:
+
+```bash
+npm i -g open-source-hero
+```
+
+Check user who you are interested in:
+
+```bash
+open-source-hero benjycui
+```
+
+## License
+
+GPL-3.0
---
+++ index.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+const username = process.argv[2] || 'benjycui';
+
+const GitHub = require('github');
+const github = new GitHub();
+
+async function getAllCommits(user) {
+  let commits = [];
+  let res = await github.search.commits({
+    q: `author:${user} merge:false`,
+    sort: 'author-date',
+    'per_page': 100,
+  });
+  commits = commits.concat(res.data.items);
+
+  while(github.hasNextPage(res)) {
+    res = await github.getNextPage(res);
+    commits = commits.concat(res.data.items);
+  }
+
+  return commits;
+}
+
+async function getAllReposContributed(user) {
+  const commits = await getAllCommits(user);
+  const repos = commits.map(({ repository }) => repository)
+          .filter(repo => !repo.fork)
+          .map(({ full_name }) => full_name)
+  const unique = {};
+  repos.forEach(r => unique[r] = true);
+  return Object.keys(unique);
+}
+
+getAllReposContributed(username)
+  .then(repos => {
+    console.log(`${username} had contributed to ${repos.length} repositories.`);
+    repos.forEach((r, i) => console.log(`${i + 1}. ${r}`));
+  })
+  .catch(e => console.error(e))
